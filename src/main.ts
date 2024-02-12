92344915a83460bbcdf86f29cfb903ec0a3af5a4
Andres Alvarez <andres15alvarez@gmail.com>
2024-02-12 10:48:10 -0400

    Test pre-commit

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,9 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-
-
 const vuetify = createVuetify({
   components,
   directives
 })
-
-
 
 createApp(App).use(vuetify).mount('#app')
