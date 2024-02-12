1517fc073a2521b9a181eb7b819339f2c4b79001
Andres Alvarez <andres15alvarez@gmail.com>
2024-02-12 11:01:42 -0400

    Check vue files

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -4,8 +4,9 @@ import DocumentationIcon from './icons/IconDocumentation.vue'
 import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
-
 import SupportIcon from './icons/IconSupport.vue'
+
+let name = 'Andres'
 </script>
 
 <template>
