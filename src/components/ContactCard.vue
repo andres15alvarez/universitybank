c054e4945e509d8d8b61dbc92535860eeee111fb
Andres Alvarez <andres15alvarez@gmail.com>
2024-03-09 18:47:40 -0400

    Create contact card component

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card elevation="16" class="text-center d-inline-block" rounded="10">
+    <div class="pa-10">
+      <v-icon class="center" :icon="icon" size="x-large" color="#085f63"></v-icon>
+      <v-card-title class="Montserrat-SemiBold my-2">{{ title }}</v-card-title>
+      <v-divider
+        class="border-opacity-100 mx-auto"
+        thickness="5"
+        color="#085f63"
+        length="55"
+      ></v-divider>
+      <v-card-text class="Montserrat-Regular my-5">
+        {{ subtitle }}
+      </v-card-text>
+    </div>
+    <v-divider class="border-opacity-100" thickness="5" color="#085f63"></v-divider>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: String,
+    title: String,
+    subtitle: String
+  }
+}
+</script>
