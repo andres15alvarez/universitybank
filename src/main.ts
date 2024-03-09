1de53db2c3e0a0b2c79cebf9157948d625e7ab5c
Andres Alvarez <andres15alvarez@gmail.com>
2024-03-09 14:54:51 -0400

    Import mdi

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/lib/iconsets/mdi.mjs'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  },
   components,
   directives
 })
