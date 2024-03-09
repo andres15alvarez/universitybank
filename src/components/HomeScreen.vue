5b7725559496290460a59218893f592f7d7935d7
Andres Alvarez <andres15alvarez@gmail.com>
2024-03-09 14:55:21 -0400

    Fix Home screen

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="container">
-    <div class="a">
+  <div class="container d-md-inline-flex mt-5 mx-5">
+    <div class="information ml-10">
       <h3 class="Montserrat-SemiBold">
         Institución comprometida con los estudiantes universitarios
       </h3>
-      <p class="Montserrat-SemiBold">soluciones financieras ágiles y eficientes</p>
+      <p class="Montserrat-SemiBold mt-5">soluciones financieras ágiles y eficientes</p>
       <div class="boton">
         <v-btn
           block
-          class="Montserrat-SemiBold text-none"
+          class="Montserrat-SemiBold ml-15 mt-5 text-none"
           variant="flat"
           color="#085F63"
           size="default"
@@ -18,54 +18,32 @@
         </v-btn>
       </div>
     </div>
-    <div class="b">
-      <img src="/ImagenHome.png" class="imagenHome" width="600" />
-    </div>
+    <img src="/ImagenHome.png" class="imagenHome w-50" width="600" />
   </div>
 </template>
 
 <style>
 .container {
-  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
-.a {
+
+.information {
   display: inline-block;
-  /* width: 55%;
-  height: 365px;
-  padding-left: 120px;
-  padding-top: 110px; */
-}
-.b {
-  display: inline-block;
-  /* width: 45%;
-  height: 365px; */
+  justify-content: center;
 }
 
 .boton {
   width: 316px;
-  margin-top: 5px;
-  margin-left: 75px;
 }
 
 h3 {
-  font-size: 48px;
+  font-size: 44px;
   color: #085f63;
 }
-
-/* .imagenHome {
-  width: 400px;
-  margin-left: -20px;
-  margin-top: -30px;
-} */
 
 p {
   font-size: 24px;
-  display: inline;
-  color: #085f63;
-}
-
-.a p {
   color: rgba(0, 0, 0, 0.575);
-  padding-left: 0%;
 }
 </style>
