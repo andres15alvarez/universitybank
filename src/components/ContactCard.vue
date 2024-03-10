ef7e62fe62ea2854d1d206ff774d4f6cc1ba79bc
Andres Alvarez <andres15alvarez@gmail.com>
2024-03-10 16:58:29 -0400

    Update component adding props

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-card elevation="16" class="text-center d-inline-block" rounded="10">
-    <div class="pa-10">
+  <v-card elevation="16" class="text-center" rounded="10" width="400" height="255">
+    <div class="pa-6">
       <v-icon class="center" :icon="icon" size="x-large" color="#085f63"></v-icon>
       <v-card-title class="Montserrat-SemiBold my-2">{{ title }}</v-card-title>
       <v-divider
@@ -9,13 +9,22 @@
         color="#085f63"
         length="55"
       ></v-divider>
-      <v-card-text class="Montserrat-Regular my-5">
+      <v-card-text class="Montserrat-Regular my-2">
         {{ subtitle }}
       </v-card-text>
     </div>
-    <v-divider class="border-opacity-100" thickness="5" color="#085f63"></v-divider>
+    <div class="dividerEnd">
+      <!-- <v-divider class="border-opacity-100" thickness="5" color="#085f63"></v-divider> -->
+    </div>
   </v-card>
 </template>
+
+<style>
+.dividerEnd {
+  display: flex;
+  align-items: end;
+}
+</style>
 
 <script>
 export default {
