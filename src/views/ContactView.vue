7bfa5a078dffb031b533b8b68d83474d729bb7fd
Andres Alvarez <andres15alvarez@gmail.com>
2024-03-10 16:58:43 -0400

    Create contact view

---
+++ src/views/ContactView.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import NavigationBar from '@/components/NavigationBar.vue'
+import ContactCard from '@/components/ContactCard.vue'
+</script>
+
+<template>
+  <NavigationBar />
+  <div class="text-center">
+    <h3>Contactanos</h3>
+  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-sheet class="d-flex justify-center">
+          <ContactCard
+            icon="mdi-home"
+            title="Dirección"
+            subtitle="Av. Universidad, Edificio Banco Universitario, piso 12, Caracas, Venezuela."
+          />
+        </v-sheet>
+      </v-col>
+      <v-col>
+        <v-sheet class="d-flex justify-center">
+          <ContactCard icon="mdi-phone" title="Teléfono" subtitle="+58 212-555-5555" />
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-sheet class="d-flex justify-center">
+          <ContactCard
+            icon="mdi-message-processing-outline"
+            title="Fax"
+            subtitle="+58 212-555-5556"
+          />
+        </v-sheet>
+      </v-col>
+      <v-col>
+        <v-sheet class="d-flex justify-center">
+          <ContactCard
+            icon="mdi-email"
+            title="Correo electrónico"
+            subtitle="info@bancouniversitario.com.ve"
+          />
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style>
+.grid {
+  display: flex;
+  justify-content: center;
+}
+</style>
