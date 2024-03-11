d1f2653eaabdd18feac645ef91c0d4ef76bd27cc
Abraham <1001.28021547.ucla@gmail.com>
2024-03-11 14:07:51 -0400

    login

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import NavegationBar from './components/NavegationBar.vue'
-import HomeScreen from './components/HomeScreen.vue'
+//import NavegationBar from './components/NavegationBar.vue'
+//import HomeScreen from './components/HomeScreen.vue'
+import LoginScreen from './components/LoginScreen.vue'
 </script>
 
 <template>
-  <NavegationBar />
-  <HomeScreen />
+  <!-- <NavegationBar />
+  <HomeScreen /> -->
+  <LoginScreen />
 </template>
