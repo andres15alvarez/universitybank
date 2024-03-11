a123b166a1332995fc64d6b5337823bc2a83c0b2
Andres Alvarez <andres15alvarez@gmail.com>
2024-03-11 13:27:16 -0400

    Create route to about us

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import ContactView from '@/views/ContactView.vue'
 import HomeView from '@/views/HomeView.vue'
+import AboutUsView from '@/views/AboutUsView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
-  { path: '/contact', name: 'contact', component: ContactView }
+  { path: '/contact', name: 'contact', component: ContactView },
+  { path: '/about', name: 'about', component: AboutUsView }
 ]
 
 const router = createRouter({
