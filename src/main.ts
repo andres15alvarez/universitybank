0f48a683ca1b4d18b79591cf6f5f804aa1485089
Andres Alvarez <andres15alvarez@gmail.com>
2024-03-13 17:58:04 -0400

    Use custom primary color

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
 import { aliases, mdi } from 'vuetify/lib/iconsets/mdi.mjs'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import router from './routers/router'
 
+const universityBankTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    primary: '#085F63'
+  }
+}
+
 const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'universityBankTheme',
+    themes: {
+      universityBankTheme
+    }
+  },
   icons: {
     defaultSet: 'mdi',
     aliases,
