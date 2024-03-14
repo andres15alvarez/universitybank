fc572200c9c991e8cd984f3cc7d2afbe58a665ca
Andres Alvarez <andres15alvarez@gmail.com>
2024-03-14 16:23:26 -0400

    Create footer bar component

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const icons = ['mdi-facebook', 'mdi-instagram', 'mdi-twitter']
+</script>
+
+<template>
+  <v-layout>
+    <v-footer class="text-center d-flex flex-column transparent">
+      <div>
+        <v-btn v-for="icon in icons" :key="icon" :icon="icon" class="mx-4" variant="text"></v-btn>
+      </div>
+      <div>Copyrigth © <strong>Banco Universitario</strong> {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </v-layout>
+</template>
+
+<style>
+.transparent {
+  background-color: transparent;
+}
+</style>
