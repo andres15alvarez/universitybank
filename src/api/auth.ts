519996adcba25a9206c633f436c6fdc8768b1d35
Andres Alvarez <andres15alvarez@gmail.com>
2024-03-23 22:17:05 -0400

    Add method to update password

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,7 +2,7 @@ import type { CreateUser, LoginResponse, UserResponse } from '@/interfaces/user'
 import { Client } from '.'
 import { objectToSnake } from 'ts-case-convert'
 
-export class AuthClient extends Client {
+export class AuthService extends Client {
   async login(email: string, password: string): Promise<LoginResponse | any> {
     const response = await this.post({
       url: 'public/client/user/login',
@@ -15,13 +15,25 @@ export class AuthClient extends Client {
     return response
   }
 
-  async create(user: CreateUser): Promise<UserResponse | any> {
+  async signup(user: CreateUser): Promise<UserResponse | any> {
     const response = await this.post({
       url: 'public/client/user/register',
       needAuthorization: false,
       data: {
         ...objectToSnake(user)
       }
+    })
+    return response
+  }
+
+  async updatePassword(oldPassword: string, newPassword: string) {
+    const response = await this.patch({
+      url: 'client/user/password',
+      needAuthorization: true,
+      data: objectToSnake({
+        password: oldPassword,
+        newPassword: newPassword
+      })
     })
     return response
   }
