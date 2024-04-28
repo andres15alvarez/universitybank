f0eb37b81d51e6fe29bee345061b3e2c8f604f9c
Andres Alvarez <andres15alvarez@gmail.com>
2024-04-28 14:14:12 -0400

    Fix responsive and add red color to debit movements

--- src/views/Dashboard/MainView.vue
+++ src/views/Dashboard/MainView.vue
@@ -3,7 +3,7 @@ import { MovementService } from '@/api/movement'
 import { UserService } from '@/api/user'
 import MainToolbar from '@/components/Dashboard/MainToolbar.vue'
 import type { Movement } from '@/interfaces/movement'
-import type { BalanceResponse } from '@/interfaces/user'
+import type { BalanceResponse, UserResponse } from '@/interfaces/user'
 import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
@@ -19,6 +19,25 @@ const userData = ref({
 })
 let balance = ref(0)
 
+function amountTextColor(multiplier: number): string {
+  return multiplier == -1 ? 'text-red' : 'text-black'
+}
+
+function getUserData() {
+  const client = new UserService()
+  client
+    .getProfile()
+    .then((response: UserResponse) => {
+      userData.value.firstName = response.firstName
+      userData.value.lastName = response.lastName
+      userData.value.accountNumber = response.accountNumber
+      userData.value.email = response.email
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
 function getBalance() {
   const client = new UserService()
   client
@@ -31,10 +50,10 @@ function getBalance() {
     })
 }
 
-function getMovements(page: number) {
+function getMovements(page: number, pageSize: number) {
   const client = new MovementService()
   client
-    .getMovements(page, 20)
+    .getMovements(page, pageSize)
     .then((response: Array<Movement>) => {
       movements.value = response
     })
@@ -44,8 +63,9 @@ function getMovements(page: number) {
 }
 
 onMounted(() => {
+  getUserData()
   getBalance()
-  getMovements(1)
+  getMovements(1, 10)
 })
 </script>
 
@@ -55,8 +75,8 @@ onMounted(() => {
     <v-container>
       <h2>Dashboard</h2>
       <v-row>
-        <v-col>
-          <v-card class="mx-auto rounded-lg" hover variant="elevated" title="Bienvenido(a)">
+        <v-col cols="12" md="6">
+          <v-card class="rounded-lg" variant="elevated" title="Bienvenido(a)">
             <template v-slot:prepend>
               <v-avatar color="secondary">
                 <v-icon color="#FFFFFF" icon="mdi-account"></v-icon>
@@ -67,8 +87,8 @@ onMounted(() => {
             </template>
           </v-card>
         </v-col>
-        <v-col>
-          <v-card class="mx-auto rounded-lg" hover variant="elevated" title="Balance">
+        <v-col cols="12" md="6">
+          <v-card class="rounded-lg" variant="elevated" title="Balance">
             <template v-slot:prepend>
               <v-avatar color="secondary">
                 <v-icon color="#FFFFFF" icon="mdi-wallet-bifold"></v-icon>
@@ -82,7 +102,7 @@ onMounted(() => {
       </v-row>
       <v-row>
         <v-col>
-          <v-card hover class="text-white rounded-lg" variant="elevated" color="secondary">
+          <v-card class="text-white rounded-lg" variant="elevated" color="secondary">
             <template v-slot:prepend>
               <v-card-title>Número de cuenta:</v-card-title>
             </template>
@@ -90,36 +110,36 @@ onMounted(() => {
               <v-card-text> {{ userData.accountNumber }}</v-card-text>
             </template>
           </v-card>
-          <div class="rounded-2xl flex-col dark:bg-slate-900/70 bg-white flex">
-            <h2>Movimientos</h2>
-            <v-table class="ma-5" fixed-header>
-              <thead>
-                <tr>
-                  <th class="text-left">Referencia</th>
-                  <th class="text-left">Cuenta</th>
-                  <th class="text-left">Descripción</th>
-                  <th class="text-left">Monto</th>
-                  <th class="text-left">Balance</th>
-                  <th class="text-left">Fecha</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="mov in movements" :key="mov.id">
-                  <td>{{ mov.id }}</td>
-                  <td>{{ mov.accountNumber }}</td>
-                  <td>{{ mov.description }}</td>
-                  <td>{{ mov.amount * mov.multiplier }}</td>
-                  <!-- Aqui pense en un v-if (para cambiar el color a rojo si era un egreso) pero dice que no se recomienda usar junto con un v-for -->
-                  <td>{{ mov.balance.toFixed(2) }}</td>
-                  <!-- .toFixed(2) fixea la cantidad con dos decimales -->
-                  <td>{{ new Date(mov.createdAt).toLocaleDateString('en-GB') }}</td>
-                </tr>
-              </tbody>
-            </v-table>
-            <v-data-table-server :items-length="10"></v-data-table-server>
-          </div>
         </v-col>
       </v-row>
+      <div class="ma-5 rounded-2xl flex-col dark:bg-slate-900/70 bg-white flex">
+        <h2>Movimientos</h2>
+        <v-table fixed-header>
+          <thead>
+            <tr>
+              <th class="text-left">Referencia</th>
+              <th class="text-left">Cuenta</th>
+              <th class="text-left">Descripción</th>
+              <th class="text-left">Monto</th>
+              <th class="text-left">Balance</th>
+              <th class="text-left">Fecha</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="mov in movements" :key="mov.id">
+              <td>{{ mov.id }}</td>
+              <td>{{ mov.accountNumber }}</td>
+              <td>{{ mov.description }}</td>
+              <td :class="amountTextColor(mov.multiplier)">
+                {{ (mov.amount * mov.multiplier).toFixed(2) }}
+              </td>
+              <td>{{ mov.balance.toFixed(2) }}</td>
+              <td>{{ new Date(mov.createdAt).toLocaleDateString('en-GB') }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+        <v-data-table-server :items-length="10"></v-data-table-server>
+      </div>
     </v-container>
   </main>
 </template>
